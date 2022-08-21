f00508845f70e7b0d4dd6e4760bc667e5b06ba7d
soheyHashimoto <heyso1209@gmail.com>
2022-08-21 12:45:46 +0900

    [MODIFY]GestureDetectorを外す

--- lib/main.dart
+++ lib/main.dart
@@ -66,36 +66,34 @@ class TodayAvailableClassroomCard extends StatelessWidget {
   const TodayAvailableClassroomCard({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: SizedBox(
-        width: 150,
-        height: 100,
-        child: Card(
+    return SizedBox(
+      width: 150,
+      height: 100,
+      child: Card(
+        child: Container(
           child: Container(
-            child: Container(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    'F301',
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                    ),
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'F301',
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
                   ),
-                  SizedBox(height: 4),
-                  Text(
-                    '14:00 - 15:00',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(height: 4),
+                Text(
+                  '14:00 - 15:00',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ],
             ),
           ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
         ),
       ),
     );
