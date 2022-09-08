4ea27f49b473be252843387a90d0d7c05bf9d02d
soheyHashimoto <heyso1209@gmail.com>
2022-09-09 02:03:33 +0900

    [ADD]SafeAreaで囲む

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -8,53 +8,55 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-return Scaffold(
-      body: Column(
-        children: <Widget>[
-          const SizedBox(height: 20),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: const Text(
-              '本日の空き教室',
-              style: TextStyle(fontWeight: FontWeight.bold),
-              textAlign: TextAlign.left,
-              textScaleFactor: 1.1,
-            ),
-          ),
-          // 左寄せ
-          SizedBox(
-            height: 100,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: const [
-                TodayAvailableClassroomCard(),
-                TodayAvailableClassroomCard(),
-                TodayAvailableClassroomCard(),
-                TodayAvailableClassroomCard(),
-              ],
-            ),
-          ),
-          const SizedBox(height: 120),
-          // button
-          ElevatedButton(
-            child: const Text(
-              '教室を検索する',
-              style: TextStyle(
-                color: Colors.black,
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            const SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: const Text(
+                '本日の空き教室',
+                style: TextStyle(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.left,
+                textScaleFactor: 1.1,
               ),
             ),
-            // 色を白に
-            style: ElevatedButton.styleFrom(
-              primary: Colors.white,
+            // 左寄せ
+            SizedBox(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  TodayAvailableClassroomCard(),
+                  TodayAvailableClassroomCard(),
+                  TodayAvailableClassroomCard(),
+                  TodayAvailableClassroomCard(),
+                ],
+              ),
             ),
-            onPressed: () => Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) {
-              return const RoomsScreen();
-            })),
-          ),
-        ],
-      ),
+            const SizedBox(height: 120),
+            // button
+            ElevatedButton(
+              child: const Text(
+                '教室を検索する',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              // 色を白に
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+              ),
+              onPressed: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) {
+                return const RoomsScreen();
+              })),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
