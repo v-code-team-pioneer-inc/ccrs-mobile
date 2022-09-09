6a6ef77e199938ee6146695c80851d18c8a2d1f5
soheyHashimoto <heyso1209@gmail.com>
2022-09-09 09:20:56 +0900

    [MODIFY]縦軸と横軸修正

--- lib/screen/rooms_screen.dart
+++ lib/screen/rooms_screen.dart
@@ -3,86 +3,88 @@ import 'package:flutter/material.dart';
 class RoomsScreen extends StatelessWidget {
   const RoomsScreen({Key? key}) : super(key: key);
 
-@override
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: const Text(
-                '本日の空き教室',
-                style: TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-                textScaleFactor: 1.1,
-              ),
+        child: Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: const Text(
+              '本日の空き教室',
+              style: TextStyle(fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+              textScaleFactor: 1.1,
             ),
-            const SizedBox(height: 20),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: DataTable(
+          ),
+          const SizedBox(height: 20),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: DataTable(
               columns: const [
                 DataColumn(
                   label: Text('時間'),
                 ),
                 DataColumn(
-                  label: Text('F100'),
+                  label: Text('09:00 ~ 10:40'),
                 ),
                 DataColumn(
-                  label: Text('F101'),
+                  label: Text('10:50 ~ 12:30'),
                 ),
                 DataColumn(
-                  label: Text('F102'),
+                  label: Text('12:30 ~ 13:20'),
                 ),
                 DataColumn(
-                  label: Text('F103'),
+                  label: Text(
+                    '13:20 ~ 15:00',
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ],
               rows: const [
                 DataRow(
                   cells: [
-                    DataCell(Text('09:00 ~ 10:40')),
-                    DataCell(Text('×')),
-                    DataCell(Text('◯')),
-                    DataCell(Text('◯')),
-                    DataCell(Text('◯')),
+                    DataCell(Text('F100')),
+                    DataCell(Center(child: Text('×'))),
+                    DataCell(Center(child: Text('◯'))),
+                    DataCell(Center(child: Text('×'))),
+                    DataCell(Center(child: Text('◯'))),
                   ],
                 ),
                 DataRow(
                   cells: [
-                    DataCell(Text('10:50 ~ 12:30')),
-                    DataCell(Text('◯')),
-                    DataCell(Text('◯')),
-                    DataCell(Text('×')),
-                    DataCell(Text('◯')),
+                    DataCell(Text('F101')),
+                    DataCell(Center(child: Text('×'))),
+                    DataCell(Center(child: Text('×'))),
+                    DataCell(Center(child: Text('×'))),
+                    DataCell(Center(child: Text('◯'))),
                   ],
                 ),
                 DataRow(
                   cells: [
-                    DataCell(Text('12:30 ~ 13:20')),
-                    DataCell(Text('◯')),
-                    DataCell(Text('×')),
-                    DataCell(Text('×')),
-                    DataCell(Text('×')),
+                    DataCell(Text('F102')),
+                    DataCell(Center(child: Text('◯'))),
+                    DataCell(Center(child: Text('◯'))),
+                    DataCell(Center(child: Text('◯'))),
+                    DataCell(Center(child: Text('◯'))),
                   ],
                 ),
                 DataRow(
                   cells: [
-                    DataCell(Text('13:20 ~ 15:00')),
-                    DataCell(Text('◯')),
-                    DataCell(Text('◯')),
-                    DataCell(Text('◯')),
-                    DataCell(Text('×')),
+                    DataCell(Text('F103')),
+                    DataCell(Center(child: Text('×'))),
+                    DataCell(Center(child: Text('◯'))),
+                    DataCell(Center(child: Text('◯'))),
+                    DataCell(Center(child: Text('◯'))),
                   ],
                 ),
               ],
             ),
-            ),
-          ],
-        ),
-      )
-    );
+          ),
+        ],
+      ),
+    ));
   }
 }
