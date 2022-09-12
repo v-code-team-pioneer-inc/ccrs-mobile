6344f940de894777b8129ddbb1968b6bbcfdc27c
soheyHashimoto <heyso1209@gmail.com>
2022-09-12 13:29:05 +0900

    [ADD]イベントを表示する

--- lib/screen/timetable_screen.dart
+++ lib/screen/timetable_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import '../models/class.dart';
+import '../logics/timetable/class_data_source.dart';
 
 class TimetableScreen extends StatelessWidget {
   const TimetableScreen({Key? key}) : super(key: key);
@@ -8,12 +10,22 @@ class TimetableScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(child:
       Scaffold(
-        body: Container(
-          child: SfCalendar(
-            view: CalendarView.workWeek,
-          )
+        body: SfCalendar(
+          view: CalendarView.workWeek,
+          dataSource: ClassDataSource(_getDataSource()),
         )
       )
     );
   }
+}
+
+List<Class> _getDataSource() {
+  final List<Class> classes = <Class>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime =
+      DateTime(today.year, today.month, today.day, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(minutes: 90));
+  classes.add(Class(
+      'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+  return classes;
 }
