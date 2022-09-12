f0a969b0848dadcae75ab71d739d00f69a95fa1a
soheyHashimoto <heyso1209@gmail.com>
2022-09-12 13:28:24 +0900

    [ADD]ClassDataSource

---
+++ lib/logics/timetable/class_data_source.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../../models/class.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class ClassDataSource extends CalendarDataSource {
+  ClassDataSource(List<Class> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].className;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
